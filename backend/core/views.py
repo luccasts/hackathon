1bdac581292b26160a06403a50a6cf820520cfe2
Matheus-Caique <matieves2004@gmail.com>
2025-08-27 00:37:24 -0300

    backend: updated code to version of OpenAI

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,12 +1,12 @@
+import json
+import os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
-import openai
-from django.conf import settings
+from openai import OpenAI
 
 # Create your views here.
 
-openai.api_key = settings.OPENAI_API_KEY
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 @csrf_exempt
 def assistant_view(request):
@@ -18,7 +18,7 @@ def assistant_view(request):
             if len(question.strip()) < 3:
                 return JsonResponse({"erro": "Pergunta muito curta"}, status=500)
             
-            response = openai.ChatCompletion.create(
+            response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
                     {"role": "system","content": "Você é um assistente educado e claro que responde dúvidas sobre comportamento infantil e educação"},
